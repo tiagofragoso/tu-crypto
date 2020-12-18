63907e2ac254856969ddf41a49fb82ec804a13cb
Tiago Fragoso <tiagofragoso@outlook.com>
2020-12-18 11:28:01 +0100

    Improve Ex. A solution for report/delivery

--- P1/solutions/a.sql
+++ P1/solutions/a.sql
@@ -5,20 +5,25 @@ CREATE TEMPORARY TABLE invalid_blocks_temp ( block_id int );
 
 CREATE OR REPLACE VIEW coinbase_transactions
 AS SELECT block_id, tx_id
-    FROM (
-             SELECT block_id, MIN(tx_id) AS tx_id
-             FROM transactions
-             GROUP BY block_id
-         ) AS first_transactions
-    JOIN inputs USING (tx_id)
-    WHERE sig_id = 0;
+FROM (
+        SELECT tx_id
+        FROM inputs
+        GROUP BY tx_id
+        HAVING COUNT(input_id) = 1
+        AND SUM(sig_id) = 0
+     ) AS one_input_transactions
+JOIN transactions USING (tx_id)
+INTERSECT
+SELECT block_id, MIN(tx_id) AS tx_id
+FROM transactions
+GROUP BY block_id;
 
 CREATE OR REPLACE VIEW non_coinbase_transactions
 AS SELECT block_id, tx_id
-    FROM transactions
-    EXCEPT
-    SELECT block_id, tx_id
-    FROM coinbase_transactions;
+FROM transactions
+EXCEPT
+SELECT block_id, tx_id
+FROM coinbase_transactions;
 
 -- 6.
 
@@ -70,7 +75,7 @@ HAVING COUNT(output_id) = 0 OR SUM(value) < 0 OR SUM(value) > 2100000000000000;
 
 -- 16.1
 
-CREATE OR REPLACE VIEW not_coinbase_inputs AS (
+CREATE OR REPLACE VIEW non_coinbase_inputs AS (
     SELECT *
     FROM non_coinbase_transactions
     JOIN inputs USING (tx_id)
@@ -79,60 +84,53 @@ CREATE OR REPLACE VIEW not_coinbase_inputs AS (
 -- 16.1.1
 
 INSERT INTO invalid_blocks_temp
--- Get blocks containing transactions where inputs have no corresponding output
+-- Get blocks containing transactions where inputs have no corresponding valid output
 SELECT DISTINCT block_id
-FROM not_coinbase_inputs
+FROM non_coinbase_inputs
 LEFT OUTER JOIN outputs USING(output_id)
-WHERE value IS NULL;
+WHERE value IS NULL OR non_coinbase_inputs.tx_id <= outputs.tx_id;
 
 -- 16.1.4
 INSERT INTO invalid_blocks_temp
 -- Get blocks where a input signature does not match the output signature
 SELECT DISTINCT block_id
-FROM not_coinbase_inputs
+FROM non_coinbase_inputs
 JOIN outputs USING (output_id)
 WHERE sig_id <> pk_id;
 
 -- 16.1.5
--- Get outputs spent more than once
 WITH double_spent_outputs AS (
+-- Get outputs spent more than once
     SELECT output_id
-    FROM not_coinbase_inputs
+    FROM non_coinbase_inputs
     JOIN outputs USING (output_id)
     GROUP BY output_id
     HAVING COUNT (input_id) > 1
 ), first_spent AS (
 -- Get the input_id of the where the output was first spent
     SELECT MIN(input_id) AS input_id, output_id
-    FROM non_coinbase_transactions
-             JOIN inputs USING (tx_id)
-    WHERE output_id IN (SELECT * FROM double_spent_outputs)
+    FROM double_spent_outputs
+    JOIN non_coinbase_inputs
+    USING (output_id)
     GROUP BY output_id
 )
 INSERT INTO invalid_blocks_temp
 -- Get blocks containing invalid inputs due to double spend
 SELECT block_id
-    FROM non_coinbase_transactions
-             JOIN inputs USING (tx_id)
-    WHERE output_id IN (SELECT * FROM double_spent_outputs)
-    AND input_id NOT IN (SELECT input_id FROM first_spent);
-
-INSERT INTO invalid_blocks_temp
--- Get blocks where there is an input referencing a later output
-SELECT DISTINCT block_id
-FROM non_coinbase_transactions
-JOIN inputs USING (tx_id)
-JOIN outputs USING (output_id)
-WHERE outputs.tx_id >= inputs.tx_id;
+    FROM double_spent_outputs
+    JOIN non_coinbase_inputs
+    USING (output_id)
+    WHERE input_id NOT IN (SELECT input_id FROM first_spent);
 
 -- 16.1.6
 
 CREATE OR REPLACE VIEW input_sums
 AS SELECT block_id, tx_id, sum
 FROM transactions JOIN (
-    SELECT not_coinbase_inputs.tx_id AS tx_id, SUM(value) AS sum
-FROM not_coinbase_inputs JOIN outputs USING (output_id)
-GROUP BY not_coinbase_inputs.tx_id) AS inputs USING(tx_id);
+    SELECT non_coinbase_inputs.tx_id AS tx_id, SUM(value) AS sum
+    FROM non_coinbase_inputs JOIN outputs USING (output_id)
+    GROUP BY non_coinbase_inputs.tx_id
+) AS inputs USING(tx_id);
 
 INSERT INTO invalid_blocks_temp
 -- Get blocks with transactions whose input sum is outside legal range
@@ -143,7 +141,8 @@ WHERE sum < 0 OR sum > 2100000000000000;
 INSERT INTO invalid_blocks_temp
 -- Get blocks with transactions whose input values are outside legal range
 SELECT DISTINCT block_id
-FROM not_coinbase_inputs JOIN outputs USING (output_id)
+FROM non_coinbase_inputs
+JOIN outputs USING (output_id)
 WHERE value < 0 OR value > 2100000000000000;
 
 -- 16.1.7
@@ -156,48 +155,52 @@ WITH output_sums AS (
 )
 INSERT INTO invalid_blocks_temp
 -- Get blocks with transactions whose sum(inputs) < sum(outputs)
-SELECT DISTINCT input_sums.block_id FROM
-input_sums JOIN output_sums USING (tx_id)
+SELECT DISTINCT input_sums.block_id
+FROM input_sums
+JOIN output_sums USING (tx_id)
 WHERE input_sums.sum < output_sums.sum;
 
 -- 16.2
 
 WITH coinbase_values AS (
     -- Get coinbase value for each block
-    SELECT block_id, SUM(value) AS coinbase_value
+    SELECT block_id, value AS coinbase_value
     FROM coinbase_transactions
-             JOIN outputs USING (tx_id)
-    GROUP BY block_id
+    JOIN outputs USING (tx_id)
 ),
  input_values AS (
-     -- Get sum(inputs) for each block
-     SELECT block_id, SUM(value) AS total_inputs
-     FROM transactions
-              JOIN inputs USING (tx_id)
-              JOIN outputs USING (output_id)
-     WHERE inputs.tx_id NOT IN (SELECT tx_id FROM coinbase_transactions)
-     GROUP BY block_id
+    -- Get sum(inputs) for each block
+    SELECT block_id, SUM(value) AS total_inputs
+    FROM transactions
+    JOIN inputs USING (tx_id)
+    JOIN outputs USING (output_id)
+    WHERE inputs.tx_id NOT IN (SELECT tx_id FROM coinbase_transactions)
+    GROUP BY block_id
  ),
  output_values AS (
-     -- Get sum(outputs) for each block
-     SELECT block_id, SUM(value) AS total_outputs
-     FROM transactions
-              JOIN outputs USING (tx_id)
-     WHERE tx_id NOT IN (SELECT tx_id FROM coinbase_transactions)
-     GROUP BY block_id
+    -- Get sum(outputs) for each block
+    SELECT block_id, SUM(value) AS total_outputs
+    FROM transactions
+    JOIN outputs USING (tx_id)
+    WHERE tx_id NOT IN (SELECT tx_id FROM coinbase_transactions)
+    GROUP BY block_id
  )
 INSERT INTO invalid_blocks_temp
 -- Get blocks where coinbase value > block creation fee + transaction fees:
 -- Block creation fee = 50 BTC; Transaction fees = sum(inputs)-sum(outputs) of all transactions
 SELECT DISTINCT block_id
-FROM coinbase_values JOIN input_values USING (block_id) JOIN output_values USING (block_id)
+FROM coinbase_values
+JOIN input_values USING (block_id)
+JOIN output_values USING (block_id)
 WHERE coinbase_values.coinbase_value > (5000000000 + (input_values.total_inputs - output_values.total_outputs));
 
-DELETE FROM invalid_blocks;
+-- [TESTING] Clear invalid_blocks table
+-- DELETE FROM invalid_blocks;
+
 INSERT INTO invalid_blocks
 SELECT DISTINCT block_id
 FROM invalid_blocks_temp;
 
--- Get invalid block count and sha256 hash
-SELECT COUNT(*) AS total, encode(sha256(string_agg(block_id::text, '' ORDER BY block_id ASC)::bytea), 'hex') AS hash
-FROM invalid_blocks;
+-- [TESTING] Get invalid block count and sha256 hash
+-- SELECT COUNT(*) AS total, encode(sha256(string_agg(block_id::text, '' ORDER BY block_id ASC)::bytea), 'hex') AS hash
+-- FROM invalid_blocks;
