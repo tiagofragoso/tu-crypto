07ecf4369c663c9f7740b8754be0aab811da97b7
Tiago Fragoso <tiagofragoso@outlook.com>
2020-12-19 12:05:07 +0000

    Improve formatting and fix Ex C. 2.a and 2.c

--- P1/solutions/c.sql
+++ P1/solutions/c.sql
@@ -7,19 +7,13 @@ DELETE FROM addressrelations;
 
 -- Joint control
 
-insert into addressrelations (addr1, addr2) SELECT I1.sig_id, I2.sig_id
-FROM inputs as I1, inputs as I2  
-WHERE I1.tx_id = I2.tx_id 
+INSERT INTO addressrelations
+SELECT I1.sig_id, I2.sig_id
+FROM inputs AS I1, inputs AS I2
+WHERE I1.tx_id = I2.tx_id
 AND I1.sig_id != I2.sig_id;
 
 /*heuristic 2*/
-
-/*insert into addressrelations (addr1, addr2) SELECT i.sig_id, o.pk_id as transactions
-FROM 	inputs as i, outputs as o
-WHERE i.tx_id = o.tx_id
-AND i.tx_id IN (SELECT tx_id from inputs GROUP BY  tx_id having count(*) = 1  ORDER BY tx_id )
-AND o.tx_id IN (SELECT tx_id from outputs GROUP BY  tx_id having count(*) = 1  ORDER BY tx_id )
-ORDER BY i.sig_id DESC;*/
 
 -- Serial control
 
@@ -41,18 +35,19 @@ WITH in1_out1 AS (
     FROM outputs
     WHERE tx_id IN (SELECT * FROM in1_out1)
 )
-INSERT INTO addressRelations
+INSERT INTO addressrelations
 SELECT in_addr, out_addr
 FROM in_addresses
-JOIN out_addresses USING (tx_id);
+JOIN out_addresses USING (tx_id)
+WHERE in_addr <> out_addr;
 
 /*create table cluster and run the clusterAddresses() function to populate it*/
+CREATE TABLE IF NOT EXISTS clusters (id int, address int);
+DELETE FROM clusters;
+INSERT INTO clusters SELECT * FROM clusteraddresses();
 
-CREATE TABLE IF NOT EXISTS clusters(id int, address int); 
-
-INSERT INTO clusters (id, address) SELECT *  FROM clusterAddresses();
-
-CREATE EXTENSION if not exists pgcrypto;
+-- SELECT encode(sha256(string_agg(id::text, ' ' ORDER BY id)::bytea), 'hex') AS hash
+-- FROM clusters;
 
 /*for debugging*/
 /*SELECT c.id, sum(o.value)
@@ -65,7 +60,7 @@ CREATE EXTENSION if not exists pgcrypto;
  8678
 */
 
-DELETE FROM max_cluster_id;
+-- DELETE FROM max_cluster_id;
 DELETE FROM max_value_by_entity;
 DELETE FROM min_addr_of_max_entity;
 DELETE FROM max_tx_to_max_entity;
@@ -75,35 +70,43 @@ DECLARE max_cluster_id integer;
 BEGIN
   
 	SELECT c.id INTO max_cluster_id
-	FROM outputs AS o, clusters as c, utxos as u
+	FROM outputs AS o, clusters AS c, utxos AS u
 	WHERE o.pk_id = c.address
 	AND o.output_id = u.output_id
 	GROUP BY c.id
-	ORDER BY sum(o.value) DESC
+	ORDER BY SUM(o.value) DESC
 	LIMIT 1;
 	
-	INSERT INTO max_value_by_entity SELECT sum(o.value) as unspent_total
-    	FROM utxos AS u, outputs as o
-    	WHERE u.output_id = o.pk_id AND o.pk_id IN 
+	INSERT INTO max_value_by_entity
+	SELECT SUM(o.value) AS unspent_total
+    	FROM utxos AS u, outputs AS o
+    	WHERE u.output_id = o.output_id AND o.pk_id IN
     		(SELECT address
         	FROM clusters
-                WHERE id = max_cluster_id);
+            WHERE id = max_cluster_id);
 
-
-	INSERT INTO min_addr_of_max_entity SELECT min(c.address)
-	FROM clusters as c
+	INSERT INTO min_addr_of_max_entity
+	SELECT MIN(c.address)
+	FROM clusters AS c
 	WHERE c.id = max_cluster_id;
 	
 	
-	INSERT INTO max_tx_to_max_entity SELECT tx_id/*, o.value*/
-	FROM outputs o, clusters c 
+	INSERT INTO max_tx_to_max_entity
+	SELECT tx_id
+	FROM outputs AS o, clusters AS c
 	WHERE c.id = max_cluster_id
-	AND c.address = o.pk_id;
+	AND c.address = o.pk_id
+	GROUP BY o.tx_id
+    ORDER BY SUM(o.value) DESC
+	LIMIT 1;
 
   
 END $$;
 
-
-
-
+-- SELECT encode(sha256(((
+--         (select value from max_value_by_entity) +
+--         (select addr from min_addr_of_max_entity) +
+--         (select tx_id from max_tx_to_max_entity))::varchar(255))
+--     ::bytea),
+--     'hex') AS hash;
 
